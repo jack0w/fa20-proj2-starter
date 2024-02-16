93cc817113f3f26793fa6291283c0003cd5f9cd0
vboxuser <vboxuser@Machine1.myguest.virtualbox.org>
2024-02-16 00:02:09 -0500

    Complete Read & Write Matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -12,6 +12,16 @@
 #   a0 (char*) is the pointer to string representing the filename
 #   a1 (int*)  is a pointer to an integer, we will set it to the number of rows
 #   a2 (int*)  is a pointer to an integer, we will set it to the number of columns
+# Registers:
+#   s0 is the pointer to string representing the filename
+#   s1 is the pointer to the number of rows
+#   s2 is the pointer to the number of columns
+#   s3 is the file descriptor, which is a unique integer tied to the file
+#   s4 is the pointer to reader buffer (This is redundant)
+#   s5 is the number of rows
+#   s6 is the number of columns
+#   t0 is the temp comparator
+
 # Returns:
 #   a0 (int*)  is the pointer to the matrix in memory
 # Exceptions:
@@ -27,16 +37,142 @@
 read_matrix:
 
     # Prologue
+	addi sp, sp, -32
+	sw s0, 0(sp)
+	sw s1, 4(sp)
+	sw s2, 8(sp)
+	sw s3, 12(sp)
+	sw s4, 16(sp)
+	sw s5, 20(sp)
+	sw s6, 24(sp)
+	sw ra, 28(sp)
 	
+	mv s0, a0
+	mv s1, a1
+	mv s2, a2
+	
+    # Set arguments for fopen
+	mv a1, s0
+	mv a2, x0
+    # Call fopen
+        jal fopen
+    # Save the returned file descriptor
+        mv s3, a0
+    # Exception handling    
+        li t0, -1
+        beq s3, t0, fopen_error
+	
+    # Set arguments for read_words: Read num of row & col in binary file
+    	mv a0, s3
+    	li a1, 2
+    # Call read_words
+    	jal read_words
+    # Save the returned pointer to reader buffer
+    	mv s4, a0
+    # Get num of row & col
+    	lw s5, 0(s4)
+    	lw s6, 4(s4)
+    # Save num of row & col in return pointers to int 
+    	sw s5, 0(s1)
+    	sw s6, 0(s2)
+    # Free the allocated memory
+    	mv a0, s4
+    	jal free
+    # Set arguments for read_words: Read the matrix
+    	mv a0, s3
+    	mul t0, s5, s6
+    	mv a1, t0
+    # Call read_words
+    	jal read_words
+    # Save the returned pointer to reader buffer
+    	mv s4, a0
 
-
-
-
-
-
-
-
+    # Set argument for fclose
+    	mv a0, s3
+    # Call fclose
+    	jal fclose
+    # Exception handling
+    	li t0, -1
+        beq a0, t0, fclose_error
+    # Copy s4 to return pointer a0
+    	mv a0, s4
+    
     # Epilogue
-
-
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw s2, 8(sp)
+	lw s3, 12(sp)
+	lw s4, 16(sp)
+	lw s5, 20(sp)
+	lw s6, 24(sp)
+	lw ra, 28(sp)
+	addi sp, sp, 32
     ret
+    
+fopen_error:
+	li a1, 90
+	j exit2
+
+malloc_error:
+	li a1, 88
+	j exit2
+
+fread_error:
+	li a1, 91
+	j exit2
+	
+fclose_error:
+	li a1, 92
+	j exit2
+
+# Arguments:
+# args:
+#   a0 = file descriptor
+#   a1 = Number of elements(integer) to be read.
+# return:
+#   a0 = pointer to the read buffer
+# registers:
+#   s0 = file descriptor
+#   s1 = Number of bytes to be read
+#   s2 = pointer to the read buffer
+
+read_words:
+    # Prologue
+    	addi sp, sp, -16
+    	sw s0, 0(sp)
+    	sw s1, 4(sp)
+    	sw s2, 8(sp)
+    	sw ra, 12(sp)
+
+	mv s0, a0
+    	slli a1, a1, 2
+	mv s1, a1
+
+    # Set arguments for malloc
+    	mv a0, s1
+    # malloc for reader buffer. Don't forget to free the allocated memory in the end!
+        jal malloc
+    # Save the returned buffer reader
+	mv s2, a0
+    # Exception handling
+	li t0, 0
+	beq s2, t0, malloc_error
+	
+    # Set arguments for fread
+    	mv a1, s0
+    	mv a2, s2
+    	mv a3, s1
+    # Call fread
+    	jal fread
+    # Exception handling
+    	bne a0, s1, fread_error
+    	mv a0, s2
+    	
+    # Epilogue
+    	lw s0, 0(sp)
+    	lw s1, 4(sp)
+    	lw s2, 8(sp)
+    	lw ra, 12(sp)
+	addi sp, sp, 16
+	
+	ret
--- src/write_matrix.s
+++ src/write_matrix.s
@@ -12,6 +12,12 @@
 #   a1 (int*)  is the pointer to the start of the matrix in memory
 #   a2 (int)   is the number of rows in the matrix
 #   a3 (int)   is the number of columns in the matrix
+# Registers:
+#   s0 is the pointer to string representing the filename
+#   s1 is the pointer to the start of the matrix in memory
+#   s2 is the number of rows in the matrix
+#   s3 is the number of columns in the matrix
+#   s4 is the file descriptor
 # Returns:
 #   None
 # Exceptions:
@@ -25,16 +31,85 @@
 write_matrix:
 
     # Prologue
+	addi sp, sp, -24
+	sw s0, 0(sp)
+	sw s1, 4(sp)
+	sw s2, 8(sp)
+	sw s3, 12(sp)
+	sw s4, 16(sp)
+	sw ra, 20(sp)
+	
+	mv s0, a0
+	mv s1, a1
+	mv s2, a2
+	mv s3, a3
+	
+    # Set arguments for fopen
+	mv a1, s0
+	li a2, 1
+    # Call fopen
+        jal fopen
+    # Save the returned file descriptor
+        mv s4, a0
+    # Exception handling    
+        li t0, -1
+        beq s4, t0, fopen_error
 
-
-
-
-
-
-
-
-
+    # Set arguments for fwrite: Write num of rows & cols
+    	mv a1, s4
+    	li a3, 2
+    	li a4, 4
+    # Use stack pointer as pointer to buffer
+    	addi sp, sp, -8
+    	sw s2, 0(sp)
+    	sw s3, 4(sp)
+    	mv a2, sp
+    # Call fwrite
+    	jal fwrite
+    # Exception handling
+    	li t0, 2
+    	bne a0, t0, fwrite_error
+    # Reset sp
+    	addi sp, sp, 8
+    
+    # Set arguments for fwrite: Write matrix
+    	mv a1, s4
+    	mv a2, s1
+    	mul t0, s2, s3
+    	mv a3, t0
+    	li a4, 4
+    # Call fwrite
+    	jal fwrite
+    # Exception handling
+    	mul t0, s2, s3
+    	bne a0, t0, fwrite_error
+    	
+    # Set argument for fclose
+    	mv a0, s4
+    # Call fclose
+    	jal fclose
+    # Exception handling
+    	li t0, -1
+        beq a0, t0, fclose_error
+    
     # Epilogue
-
-
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw s2, 8(sp)
+	lw s3, 12(sp)
+	lw s4, 16(sp)
+	lw ra, 20(sp)
+	addi, sp, sp, 24
     ret
+    
+fopen_error:
+	li a1, 93
+	j exit2
+
+fwrite_error:
+	li a1, 94
+	j exit2
+	
+fclose_error:
+	li a1, 95
+	j exit2
